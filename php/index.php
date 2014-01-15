e93226b354feea20cb6078eb5823a3f866232fd2
longbill.cn@gmail.com <liuchun643210>
2014-01-15 16:46:59 +0800

    fixed bugs

--- php/index.php
+++ php/index.php
@@ -4,7 +4,8 @@ include('endskin.php');
 
 $t = new EndSkin('templates/');
 
-$data = array();
+$data = array('page_type'=>'home');
+$t->assign($data);
 $s = $t->result('index.html');
 file_put_contents('../index-real.html',$s);
 echo "index-real.html\n";
