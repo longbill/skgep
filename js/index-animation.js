a7fd28eae3623bb0424109948e8dbc3859159961
longbill.cn@gmail.com <liuchun643210>
2014-02-27 02:46:48 +0800

    update parallax effects

--- js/index-animation.js
+++ js/index-animation.js
@@ -1,11 +1,14 @@
 $(function()
 {
+	if (SK.isTouchDevice) return;
+	var H = $('html').height();
+	var h = $(window).height();
 	$(window).bind('scroll',function()
 	{
 		var y = $(window).scrollTop();
 		for(var i=0,ani; ani=ANI.animations[i];i++)
 		{
-			if (ani.canRun(y))
+			if (ani.canRun(y,h,H))
 			{
 				ani.run.call(ani);
 			}
@@ -115,6 +118,58 @@ ANI.animations[2] =
 				var dim = $(self).data();
 				$(self).animate(dim,300);
 			},(ani.EN ? (3-i) : i )*200);
+		});
+	}
+};
+
+
+
+
+ANI.animations[3] = 
+{
+	doms: null,
+	EN:false,
+	init: function()
+	{
+		this.doms = $('#animation-block1').add('#animation-block2').add('#animation-block3');
+		this.doms.each(function()
+		{
+			$(this).data(
+			{
+				'top':'0',
+				'opacity':1
+			});
+			$(this).css(
+			{
+				'position':'relative',
+				'top':500,
+				'opacity':0
+			});
+		});
+		this.EN = $('html').is('.EN');
+	},
+	animated: false,
+	canRun: function(y,h,H)
+	{
+		if (this.animated) return false;
+		if (H - y - h < 200)
+		{
+			this.animated = true;
+			return true;
+		}
+		return false;
+	},
+	run : function()
+	{
+		var ani = this;
+		this.doms.each(function(i)
+		{
+			var self = this;
+			setTimeout(function()
+			{
+				var dim = $(self).data();
+				$(self).animate(dim,300);
+			},(ani.EN ? i : (3-i) )*300);
 		});
 	}
 };
